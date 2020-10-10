6dac02d9d7fe58e25dbd574f610426064307b89a
rafael <rafaelllparaynooo23@gmail.com>
2020-10-10 17:20:26 +0800

    fixing

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,11 +8,11 @@ const navigationItems = () => {
     <ul className={classes.NavigationItems}>
       <NavigationItems link="/" exact>
         <i className="fa fa-lg fa-fw fa-calendar"></i>{" "}
-        <span className="menu-item-parent">Calendar</span>
+        <span className="menu-item-parent">Dashboard</span>
       </NavigationItems>
       <NavigationItems link="/address">
         <i className="fa fa-lg fa-fw fa-table"></i>{" "}
-        <span className="menu-item-parent">Adress Book</span>
+        <span className="menu-item-parent">Users</span>
       </NavigationItems>
 
       <NavigationItems link="/contacts">
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,28 +4,41 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
-const sideDrawer = props => {
-    let attachedClasses = [classes.SideDrawer, classes.Close];
-    if (props.open) {
-        attachedClasses = [classes.SideDrawer, classes.Open];
-    }
+  return (
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          {/* <Logo /> */}
+          <h2>E-arrival</h2>
+        </div>
+        <div className={classes.info}>
+          <img
+            style={{ maxWidth: "80px" }}
+            className="img-thumbnail img-fluid"
+            src={require("../../../assets/images/1.png")}
+            alt="user"
+          />
+          <div className={classes.personal}>
+            <span>Rafael Parayno</span>
+            <span>Administrator</span>
+          </div>
 
-    return (
-        <>
-            <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(" ")}>
-                <div className={classes.Logo}>
-                    {/* <Logo /> */}
-                   <h2>E-arrivaL</h2> 
-                </div>
-                <nav>
-                    <NavigationItems></NavigationItems>
-                </nav>
-            </div>
-        </>
-    );
+          <div className={classes.textSpan}></div>
+        </div>
 
-}
+        <nav>
+          <NavigationItems></NavigationItems>
+        </nav>
+      </div>
+    </>
+  );
+};
 
 export default sideDrawer;
